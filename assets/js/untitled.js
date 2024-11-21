6ba5095b532e0631782fc3ade93cddfcb953e78a
Kailash-Pavesoft <kailashsuthar.pavesoft@gmail.com>
2024-11-21 20:28:18 +0530

    updated cards ui

--- assets/js/untitled.js
+++ assets/js/untitled.js
@@ -9,3 +9,24 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+
+document.querySelectorAll('.scroll-link').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent default anchor click behavior
+    const targetId = this.getAttribute('href').substring(1); // Get the target ID
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      // Calculate the offset
+      const offset = -100; // Adjust this value for your desired top spacing
+      const targetPosition = targetElement.offsetTop - offset;
+
+      // Smooth scroll to the position
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth',
+      });
+    }
+  });
+});
